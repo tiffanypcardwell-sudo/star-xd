2da2744f7bafda329c79a0aa92c98c7ff7dd24fd
tiffanypcardwell-sudo <tiffanypcardwell@gmail.com>
2026-05-06 01:15:14 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@
 export default {
   // PUT YOUR SESSION ID BELOW (Required)
   // Get from: https://star.malvintech.sbs/
-  SESSION_ID: "",
+  SESSION_ID: "king~H4sIAAAAAAAAA5VUW5OiOBT+L3lta1ouCljVVYuIqAgIiBe25iFCQBASJFGkp/zvU9jd0/OwO9vLU0hS53znu+QHwCSjyEQtGP0AVZ1dIUPdkrUVAiMwviQJqkEPxJBBMALN6iqGr0WxCMJ2Gjv7cjMWWs6xFkkSsO1W25+xNFGEdK7TF3DvgepyKLLoDwX769lw4bo3kZ8HW98hyJzPck+KX4fPLIf7k3VxPJsi9bZJX8C9qwizOsOpXh1RiWpYmKhdwaz+GnzRtmz7PC+XUyeFu50vH+iusCwpj9xMtLEhJsjQ96frjet/Db5EhuvdWZ6dZFtq/UoJl36lcjfWGCfBslVvWm+bsznp19r8DT7NUozieYwwy1j7Zd5b85C9EikrhMLA7jAqZ0tNGZQXOQkUNtf1TeUg4svILuWvAWei4i+sSuqHjhoSwwrD0jySZV7AYdIInuTtmgZjQxjg4Hfgq/rDK6f/w/vWFM6CPCmHac40l23HkV7ISsY9PYVxvdaUeTD2pnk722+/CN+bQGFpa+nYhcGM7k5epMyrwbScvU5oPjascX7a3nZ2aB/FT/iQXeo/odzL5FBNoHALW+fAt3bNVYu01Ll0aTr0CNfP02RsHUzfFkWXJ1xZuo54uh34YsutixtDukdvmCAlUZNQbANcNaaipS+PiU6oncdgxN17oEZpRlkNWUZwtzcQewDGVx9FNWIPdoFLpxd+k5pFut2n3K7lAnIm6+vinOZ7j8yep94eC0UeSiF5AT1Q1SRClKJ4llFG6tZClMIUUTD6+3sPYHRjb7p13QSuB5KspizAl6ogMP4Q9eMQRhG5YOa3ONK6BarBqP+5jRjLcEo7Gi8Y1tExuyLtCBkFowQWFP0aENUoBiNWX9Cv0Gok7nh3XN/gx7oOeqB86JHFYAR4QeCHw6E05AfyiBf/ot+ariysqm8YMdADxeMaJ/PSgBOGnKTI7ze7gx7AsCsGGnhFNI46zt8hdx1ixGBWUDACmp0rkq1OdNuohlQ2DFVPVS1VweeIH1Z502LhX4/cuJ7FYyOc+bdFZWzR5CndNDd9QFTn2FfQarPL7Kd+8/IPRbqsVY69y/L9a7U1pZkoX7PnNpps/bZoWmmBKxH11VpfqJ5WhFlrIU8y9Ut/V1dpkpibwKfJcGy4583RaeV2Oc03bDdV3ZeuW4yuWYR+b4bJ1Y35G+a3a8JHshyuYvU6KNdI5TZa2fiCzbvV3KXKqtiF0mFellOcB03k8akYhLhAhnU6mEf7GdtS6zyfjfTQjtU3Ez9CVLw/XtnDX5143W+Socdb8K7Cf4r5BrzzXP/e+63G++vyLwkd+0Etxi68Ca5Xen3qQ6/A6TOZRpbnQHVC4tUu5zZinjAL3O/fe6AqIEtIXYIRgDiuycMqNbl0Jp7jhPyhmaae5mqTLrvJC0iZ+hmMdVYiymBZgREnSXKfVyRZ6YGyVavKZ5B95Amojy/QwP0nDu95N2oHAAA=",
 
   // PUT YOUR REMOVE BACKGROUND KEY (Optional)
   // Get from: https://remove.bg/api
@@ -45,14 +45,14 @@ export default {
   BOT_NAME: "sᴛᴀʀ-xᴅ",
 
   // Add Owner WhatsApp Number (without +)
-  OWNER_NUMBER: "263725787000",
+  OWNER_NUMBER: "233266676258",
 
   // Command Mode: "public" or "private"
   // private = only owner can use commands
   COMMAND_MODE: "private",
 
   // Owner Name
-  BOT_OWNER: "ᴍʀ xᴅᴋɪɴɢ",
+  BOT_OWNER: "BÎG 🧋 FÅMÊ 🧃,
 
   // Timezone
   TIMEZONE: "Africa/Harare"
